23d8fcfa5e925cad900876cdc9a3ad90c096aa38
Roger Pence <roger.pence@gmail.com>
2017-10-23 16:48:34 -0500

    working exept for event removal

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 var mytags = document.querySelectorAll('.tag-x');
 
+function removeTag(ele) {
+    ele.removeEventListener('click', () => { removeTag(mytag); });
+    //ele.parentElement.parentElement.removeChild(ele.parentElement);
+}
+
 [].forEach.call(mytags, function(mytag) {
-    mytag.addEventListener('click', function(e) {
-
-        // Should event handler be removed on parent before delete?             
-        this.parentElement.parentElement.removeChild(this.parentElement);
-
-    }, false);
+    mytag.addEventListener('click', () => { removeTag(mytag); }, false);
 });
 
-document.querySelector('.tag-input').addEventListener('keypress', function(ev) {
+document.querySelector('.tag-input').addEventListener('keydown', function(ev) {
     const TABKEY = 9;
 
     let e = ev || window.event;
 
     if (e.keyCode == TABKEY) {
-        // Don't allow blank entry.
         if (this.value.trim() == '') {
             e.preventDefault();
             this.focus();
             return false;
         }
-        console.log(e.keyCode);
 
         e.preventDefault();
         insertTag(this.value);
@@ -32,11 +30,22 @@ document.querySelector('.tag-input').addEventListener('keypress', function(ev) {
 });
 
 function insertTag(tag) {
-    const temp = '<span class="tag">{{tag}}<a href="#" class="tag-x"><i class="fa fa-trash"></i></a></span>';
+    const temp = '<span class="tag">{{tag}}<a data-tag="{{tag}}" href="#" class="tag-x"><i class="fa fa-trash"></i></a></span>';
 
     ele = document.querySelector('.tag-input');
 
-    let html = temp.replace('{{tag}}', tag);
-
+    let html = temp.replace(/{{tag}}/g, tag);
     ele.insertAdjacentHTML('beforebegin', html);
+
+    let mytag = document.querySelector('a[data-tag="' + tag + '"');
+    mytag.addEventListener('click', () => { removeTag(mytag); }, false);
 }
+
+function insertInitialTags(initialTags) {
+    initialTags.forEach(function(tag) {
+        insertTag(tag);
+    });
+};
+
+const initialTags = ['php', 'laravel'];
+insertInitialTags(initialTags);
