56cdebe2adbbf9b6b3e8c565f6b6bcee394b64c6
Roger Pence <roger.pence@gmail.com>
2017-10-23 14:33:16 -0500

    complete deleting a tag

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ var mytags = document.querySelectorAll('.tag-x');
 [].forEach.call(mytags, function(mytag) {
 
     mytag.addEventListener('click', function(e) {
-        console.log('clicked');
-        console.log(this.parentElement.innerHTML);
-
-        this.parentElement.removeChild(this);
+        this.parentElement.parentElement.removeChild(this.parentElement); 
     }, false);
 
 });
