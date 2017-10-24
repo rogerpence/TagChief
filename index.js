075a7f71a900aafecce517e3349edc4fe127e373
Roger Pence <roger.pence@gmail.com>
2017-10-23 22:49:00 -0500

    minor renaming

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.querySelector('input.tag-text-input').addEventListener('keydown', funct
 
 function insertTag(tag) {
     const REMOVE_HANDLER_AFTER_FIRST_USE = true;
-    const template = '<span id="span-{{tag}}" class="tag">{{tag}}<a href="#" class="tag-x"><i data-tag="span-{{tag}}" class="fa fa-trash"></i></a></span>';   
+    const template = '<span id="tag-{{tag}}" class="tag">{{tag}}<a href="#" class="tag-x"><i data-tag="tag-{{tag}}" class="fa fa-trash"></i></a></span>';   
     let html = template.replace(/{{tag}}/g, tag);
 
     // Insert new tag html immediately before tag
@@ -28,7 +28,7 @@ function insertTag(tag) {
     ele.insertAdjacentHTML('beforebegin', html);
 
     // Assign delete tag action on tag click. 
-    let mytag = document.getElementById('span-' + tag);
+    let mytag = document.getElementById('tag-' + tag);
     mytag.addEventListener('click', 
         function(e) {
             var tagOwnerId = e.target.getAttribute('data-tag');
